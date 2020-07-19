dd66d5fb86eeed335a44d7ecccac58b673802d39
KaiN <kain@piwnica.ws>
2020-07-19 15:11:13 +0200

    formatting

--- gameover.c
+++ gameover.c
@@ -13,20 +13,20 @@ static tSimpleBufferManager *s_pVpManager;
 static tBitMap *s_pGameOver;
 
 extern tState g_sStateMenu;
-extern tStateManager *g_pStateMachineGame; 
+extern tStateManager *g_pStateMachineGame;
 
 
 void stateGameOverCreate(void){
- 
+
 s_pView = viewCreate(0,
-    TAG_VIEW_COPLIST_MODE, COPPER_MODE_BLOCK, 
-    TAG_VIEW_GLOBAL_CLUT, 1, 
-    TAG_END 
+    TAG_VIEW_COPLIST_MODE, COPPER_MODE_BLOCK,
+    TAG_VIEW_GLOBAL_CLUT, 1,
+    TAG_END
 );
 
 s_pVp = vPortCreate(0,
-    TAG_VPORT_VIEW, s_pView, 
-    TAG_VPORT_BPP, 5, 
+    TAG_VPORT_VIEW, s_pView,
+    TAG_VPORT_BPP, 5,
     TAG_END
 );
 // Paleta z falkona
@@ -36,42 +36,41 @@ paletteLoad("data/falkon.plt", s_pVp->pPalette, 32);
 s_pGameOver = bitmapCreateFromFile("data/gej_ower.bm", 0);
 
 s_pVpManager = simpleBufferCreate(0,
-    TAG_SIMPLEBUFFER_VPORT, s_pVp, 
-    TAG_SIMPLEBUFFER_BITMAP_FLAGS, BMF_CLEAR | BMF_INTERLEAVED, 
-    TAG_SIMPLEBUFFER_IS_DBLBUF, 0, 
+    TAG_SIMPLEBUFFER_VPORT, s_pVp,
+    TAG_SIMPLEBUFFER_BITMAP_FLAGS, BMF_CLEAR | BMF_INTERLEAVED,
+    TAG_SIMPLEBUFFER_IS_DBLBUF, 0,
     TAG_END
 );
 
 
-bitmapLoadFromFile(s_pVpManager->pBack, "data/gej_ower.bm", 0, 0); 
+bitmapLoadFromFile(s_pVpManager->pBack, "data/gej_ower.bm", 0, 0);
 
-systemUnuse(); 
-joyOpen(); 
+systemUnuse();
+joyOpen();
 keyCreate();
 viewLoad(s_pView);
 
 }
 
 void stateGameOverLoop(void){
-    joyProcess();
+	joyProcess();
 	keyProcess();
-    if(joyUse(JOY1_FIRE) || keyUse(KEY_RETURN)) {
+
+	if(joyUse(JOY1_FIRE) || keyUse(KEY_RETURN)) {
 		stateChange(g_pStateMachineGame, &g_sStateMenu);
-    return;
-	
+		return;
+  }
 
-    viewProcessManagers(s_pView); 
-	copProcessBlocks(); 
-	vPortWaitForEnd(s_pVp); 
-
-    }
+	viewProcessManagers(s_pView);
+	copProcessBlocks();
+	vPortWaitForEnd(s_pVp);
 }
 
 void stateGameOverDestroy(void){
-    joyClose();
+	joyClose();
 	keyDestroy();
-    viewDestroy(s_pView); 
-    bitmapDestroy(s_pGameOver);
+	viewDestroy(s_pView);
+	bitmapDestroy(s_pGameOver);
 }
 
 
