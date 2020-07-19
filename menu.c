470ffab6168ee7e4e962879f58035c1d6237d0dd
KaiN <kain@piwnica.ws>
2020-07-19 15:10:58 +0200

    s_pTitle is not needed in menu.c - we're loading into screen bitmap

--- menu.c
+++ menu.c
@@ -11,71 +11,64 @@
 static tView *s_pView;
 static tVPort *s_pVp;
 static tSimpleBufferManager *s_pVpManager;
-static tBitMap *s_pTitle;
 
 extern tState g_sStateGame;
-extern tStateManager *g_pStateMachineGame; 
+extern tStateManager *g_pStateMachineGame;
 
 
 void stateMenuCreate(void){
- 
-s_pView = viewCreate(0,
-    TAG_VIEW_COPLIST_MODE, COPPER_MODE_BLOCK, 
-    TAG_VIEW_GLOBAL_CLUT, 1, 
-    TAG_END 
-);
+	s_pView = viewCreate(0,
+		TAG_VIEW_COPLIST_MODE, COPPER_MODE_BLOCK,
+		TAG_VIEW_GLOBAL_CLUT, 1,
+		TAG_END
+	);
+	s_pVp = vPortCreate(0,
+		TAG_VPORT_VIEW, s_pView,
+		TAG_VPORT_BPP, 5,
+		TAG_END
+	);
 
-s_pVp = vPortCreate(0,
-    TAG_VPORT_VIEW, s_pView, 
-    TAG_VPORT_BPP, 5, 
-    TAG_END
-);
-// Paleta z falkona
-paletteLoad("data/falkon.plt", s_pVp->pPalette, 32);
+	// Paleta z falkona
+	paletteLoad("data/falkon.plt", s_pVp->pPalette, 32);
 
-
-s_pTitle = bitmapCreateFromFile("data/title.bm", 0);
-
-s_pVpManager = simpleBufferCreate(0,
-    TAG_SIMPLEBUFFER_VPORT, s_pVp, 
-    TAG_SIMPLEBUFFER_BITMAP_FLAGS, BMF_CLEAR | BMF_INTERLEAVED, 
-    TAG_SIMPLEBUFFER_IS_DBLBUF, 0, 
-    TAG_END
-);
+	s_pVpManager = simpleBufferCreate(0,
+			TAG_SIMPLEBUFFER_VPORT, s_pVp,
+			TAG_SIMPLEBUFFER_BITMAP_FLAGS, BMF_CLEAR | BMF_INTERLEAVED,
+			TAG_SIMPLEBUFFER_IS_DBLBUF, 0,
+			TAG_END
+	);
 
 
-bitmapLoadFromFile(s_pVpManager->pBack, "data/title.bm", 0, 0); 
-joyOpen(); 
-keyCreate();
-
-systemUnuse(); 
-
-viewLoad(s_pView);
-
+	bitmapLoadFromFile(s_pVpManager->pBack, "data/title.bm", 0, 0);
+	joyOpen();
+	keyCreate();
+	systemUnuse();
+	viewLoad(s_pView);
 }
 
 void stateMenuLoop(void){
-    joyProcess();
+	joyProcess();
 	keyProcess();
+
 	if(keyUse(KEY_ESCAPE)) {
 		gameExit();
+		return;
 	}
-    if(joyUse(JOY1_FIRE) || keyUse(KEY_RETURN)) {
+
+	if(joyUse(JOY1_FIRE) || keyUse(KEY_RETURN)) {
 		stateChange(g_pStateMachineGame, &g_sStateGame);
-    return;
+		return;
 	}
 
-    viewProcessManagers(s_pView); 
-	copProcessBlocks(); 
-	vPortWaitForEnd(s_pVp); 
-
+	viewProcessManagers(s_pView);
+	copProcessBlocks();
+	vPortWaitForEnd(s_pVp);
 }
 
 void stateMenuDestroy(void){
-    viewDestroy(s_pView); 
+	viewDestroy(s_pView);
 	joyClose();
 	keyDestroy();
-    bitmapDestroy(s_pTitle);
 }
 
 
