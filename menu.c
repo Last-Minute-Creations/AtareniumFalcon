ae2e0808d32aadc49b701ed953dfc15d456b2094
Luc3k <itslucus@gmail.com>
2021-03-24 17:44:15 +0100

    primitive starfield in main menu

--- menu.c
+++ menu.c
@@ -7,10 +7,13 @@
 #include <ace/utils/font.h>
 #include <stdio.h>
 #include <ace/managers/state.h>
+#include <ace/utils/chunky.h>
 
 static tView *s_pView;
 static tVPort *s_pVp;
 static tSimpleBufferManager *s_pVpManager;
+
+#define STAR_COUNT 12
 
 extern tState g_sStateGame;
 extern tState g_sStateIntro;
@@ -19,29 +22,170 @@ extern tStateManager *g_pStateMachineGame;
 
 UBYTE creditsControl = 0;
 
+UBYTE pxColor[] = {1, 2, 3, 4, 5};
+UBYTE bgColor = 0;
 
-void stateMenuCreate(void){
+UBYTE starTick1 = 0;
+UBYTE starCount1 = 0;
+UBYTE starTick2 = 0;
+UBYTE starCount2 = 0;
+UBYTE starPrev1 = 0;
+UBYTE starPrev2 = 0;
+
+UWORD p1x[STAR_COUNT][5] = {    // x dla pierwszej grupy gwiazdek
+
+	{158, 153, 120, 80, 5},
+	{154, 144, 110, 84, 33},
+	{152, 146, 142, 138, 132},
+
+	{162, 167, 200, 260, 315},
+	{166, 178, 193, 216, 246},
+	{170, 190, 228, 265, 310},
+
+	{164, 176, 200, 260, 315},
+	{160, 165, 170, 175, 180},
+	{174, 190, 228, 265, 310},
+
+	{158, 153, 120, 80, 5},
+	{154, 144, 110, 84, 14},
+	{152, 140, 128, 116, 100},
+};
+UWORD p1y[STAR_COUNT][5] = { // y dla pierwszej grupy gwiazdek
+	{124, 122, 120, 118, 116},
+	{120, 100, 80, 60, 40},
+	{121, 105, 96, 42, 2},
+
+	{124, 122, 120, 118, 116},
+	{120, 100, 82, 62, 10},
+	{118, 105, 96, 42, 2},
+
+	{130, 158, 180, 204, 250},
+	{140, 155, 172, 200, 246},
+	{148, 146, 144, 142, 140},
+
+	{130, 158, 180, 204, 245},
+	{140, 141, 142, 144, 146},
+	{148, 160, 185, 210, 240},
+};
+
+UWORD p2x[STAR_COUNT][5] = {    // x dla drugiej grupy gwiazdek
+
+	{156, 125, 100, 64, 12},
+	{142, 122, 92, 52, 6},
+	{138, 129, 121, 116, 110},
+
+	{160, 175, 200, 260, 315},
+	{162, 178, 186, 210, 232},
+	{164, 190, 228, 265, 310},
+
+	{167, 177, 202, 263, 318},
+	{165, 169, 178, 185, 194},
+	{171, 188, 225, 261, 312},
+
+	{155, 140, 120, 80, 17},
+	{149, 115, 80, 57, 9},
+	{158, 138, 122, 101, 78},
+};
+UWORD p2y[STAR_COUNT][5] = { // y dla drugiej grupy gwiazdek
+	
+	{120, 114, 110, 106, 100},
+	{127, 100, 70, 40, 10},
+	{122, 101, 65, 33, 2},
+
+	{118, 114, 110, 106, 100},
+	{127, 100, 70, 40, 10},
+	{122, 101, 65, 33, 2},
+
+	{133, 157, 177, 201, 247},
+	{145, 153, 170, 198, 241},
+	{149, 147, 145, 144, 142},
+
+	{135, 155, 180, 214, 250},
+	{143, 153, 171, 198, 241},
+	{146, 145, 143, 141, 140},
+};
+
+
+
+void pixel1(void)
+{
+
+	if (starTick1 == 0)
+	{
+		for (UBYTE i = 0; i < STAR_COUNT; ++i)
+		{
+			chunkyToPlanar(pxColor[0], p1x[i][0], p1y[i][0], s_pVpManager->pBack);
+		}
+	}
+	if (starTick1 == 7)
+	{
+		starPrev1 = starCount1;
+		++starCount1;
+
+		for (UBYTE i = 0; i < STAR_COUNT; ++i)
+		{
+			chunkyToPlanar(bgColor, p1x[i][starPrev1], p1y[i][starPrev1], s_pVpManager->pBack);
+			chunkyToPlanar(pxColor[starCount1], p1x[i][starCount1], p1y[i][starCount1], s_pVpManager->pBack);
+		}
+		starTick1 = 1;
+	}
+	if (starCount1 == 5)
+	{
+		starTick1 = 5;
+		starCount1 = 0;
+		starPrev1 = 0;
+	}
+}
+
+void pixel2(void)
+{
+
+	if (starTick2 == 0)
+	{
+		for (UBYTE i = 0; i < STAR_COUNT; ++i)
+		{
+			chunkyToPlanar(pxColor[0], p2x[i][0], p2y[i][0], s_pVpManager->pBack);
+		}
+	}
+	if (starTick2 == 11)
+	{
+		starPrev2 = starCount2;
+		++starCount2;
+
+		for (UBYTE i = 0; i < STAR_COUNT; ++i)
+		{
+			chunkyToPlanar(bgColor, p2x[i][starPrev2], p2y[i][starPrev2], s_pVpManager->pBack);
+			chunkyToPlanar(pxColor[starCount2], p2x[i][starCount2], p2y[i][starCount2], s_pVpManager->pBack);
+		}
+		starTick2 = 1;
+	}
+	if (starCount2 == 5)
+	{
+		starTick2 = 8;
+		starCount2 = 0;
+		starPrev2 = 0;
+	}
+}
+
+void stateMenuCreate(void)
+{
 	s_pView = viewCreate(0,
-		TAG_VIEW_COPLIST_MODE, COPPER_MODE_BLOCK,
-		TAG_VIEW_GLOBAL_CLUT, 1,
-		TAG_END
-	);
+						 TAG_VIEW_COPLIST_MODE, COPPER_MODE_BLOCK,
+						 TAG_VIEW_GLOBAL_CLUT, 1,
+						 TAG_END);
 	s_pVp = vPortCreate(0,
-		TAG_VPORT_VIEW, s_pView,
-		TAG_VPORT_BPP, 5,
-		TAG_END
-	);
+						TAG_VPORT_VIEW, s_pView,
+						TAG_VPORT_BPP, 5,
+						TAG_END);
 
 	// Paleta z falkona
 	paletteLoad("data/falkon.plt", s_pVp->pPalette, 32);
 
 	s_pVpManager = simpleBufferCreate(0,
-			TAG_SIMPLEBUFFER_VPORT, s_pVp,
-			TAG_SIMPLEBUFFER_BITMAP_FLAGS, BMF_CLEAR | BMF_INTERLEAVED,
-			TAG_SIMPLEBUFFER_IS_DBLBUF, 0,
-			TAG_END
-	);
-
+									  TAG_SIMPLEBUFFER_VPORT, s_pVp,
+									  TAG_SIMPLEBUFFER_BITMAP_FLAGS, BMF_CLEAR | BMF_INTERLEAVED,
+									  TAG_SIMPLEBUFFER_IS_DBLBUF, 0,
+									  TAG_END);
 
 	bitmapLoadFromFile(s_pVpManager->pBack, "data/title.bm", 0, 0);
 	joyOpen();
@@ -52,25 +196,34 @@ void stateMenuCreate(void){
 	creditsControl = 1;
 }
 
-void stateMenuLoop(void){
+void stateMenuLoop(void)
+{
 	joyProcess();
 	keyProcess();
+	++starTick1;
+	++starTick2;
+	pixel1();
+	pixel2();
 
-	if(keyUse(KEY_ESCAPE)) {
+	if (keyUse(KEY_ESCAPE))
+	{
 		gameExit();
 		return;
 	}
 
-	if(joyUse(JOY1_FIRE) || keyUse(KEY_RETURN)) {
+	if (joyUse(JOY1_FIRE) || keyUse(KEY_RETURN))
+	{
 		stateChange(g_pStateMachineGame, &g_sStateGame);
 		return;
 	}
-	
-	if(keyUse(KEY_I)) {
+
+	if (keyUse(KEY_I))
+	{
 		stateChange(g_pStateMachineGame, &g_sStateIntro);
 		return;
 	}
-	if(keyUse(KEY_C)) {
+	if (keyUse(KEY_C))
+	{
 		stateChange(g_pStateMachineGame, &g_sStateCredits);
 		return;
 	}
@@ -80,19 +233,18 @@ void stateMenuLoop(void){
 	vPortWaitForEnd(s_pVp);
 }
 
-void stateMenuDestroy(void){
+void stateMenuDestroy(void)
+{
 	systemUse();
 	viewDestroy(s_pView);
 	joyClose();
 	keyDestroy();
 }
 
-
 tState g_sStateMenu = {
-  .cbCreate = stateMenuCreate,
-  .cbLoop = stateMenuLoop,
-  .cbDestroy = stateMenuDestroy,
-  .cbSuspend = 0,
-  .cbResume = 0,
-  .pPrev = 0
-};
+	.cbCreate = stateMenuCreate,
+	.cbLoop = stateMenuLoop,
+	.cbDestroy = stateMenuDestroy,
+	.cbSuspend = 0,
+	.cbResume = 0,
+	.pPrev = 0};
